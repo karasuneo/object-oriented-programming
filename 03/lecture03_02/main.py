890ff3eedc5775334c92a19cbf4822671c4492ec
Emma Paris <yusei4520030401@gmail.com>
2022-10-13 15:51:22 +0900

    03の完成

---
+++ 03/lecture03_02/main.py
@@ -0,0 +1,7 @@
+from my_module.K21116.lecture03_hero import Hero
+from my_module.K21116.lecture03_magician2 import Magician2
+
+hero = Hero(name="愛知太郎", hp=1000)
+print(hero)
+magician = Magician2(name="愛知次郎", hp=200, mp=100)
+print(magician)
