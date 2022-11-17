7b30cae7183bbdc71a9a6c70ebef78d9b5e011d7
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-17 13:51:38 +0900

    講義ファイルの追加

---
+++ 07/lecture07_ImageGradients.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+img = cv2.imread('uchitane_far.png',cv2.IMREAD_GRAYSCALE)
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+#print(laplacian[0:8,0:8])
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+plt.show()
