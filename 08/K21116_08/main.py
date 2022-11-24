b998e1ba3bc7faa69418004d2870a2a2c0c195ba
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-24 15:34:27 +0900

    課題ファイルの追加

---
+++ 08/K21116_08/main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import random # ランダムデータ作成のためのライブラリ
+
+app = Flask(__name__)
+
+# 1. プロジェクトのトップ（じゃんけんアプリや、課題のアプリへのリンクを配置するだけ）
+@app.route('/')
+def index():
+      return render_template('index.html')
+
+
+# 2. じゃんけんアプリの入力フォーム
+@app.route('/janken')
+def janken():
+      # じゃんけんの入力画面のテンプレートを呼び出し
+      return render_template('janken_form.html')
+
+
+# 3. じゃんけんデータ送信先とじゃんけん結果表示画面
+@app.route('/janken/play', methods=["POST"])
+def janken_play():
+      pass # 関数の中身を空にできないので、pass構文を使って埋めてます
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
